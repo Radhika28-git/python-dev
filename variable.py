002a1fcc2168d5b49d785ac02450900a1b8a8643
Radhika Patel <radhikapatel7974@gmail.com>
2025-09-14 12:26:21 +0530

    Add files via upload

---
+++ variable.py
@@ -0,0 +1,22 @@
+#string()
+name="radhika"
+print(type(name))
+
+#integer
+roll_number= 165
+print(type(roll_number))
+
+#floating number
+percentage= 79
+print(type(percentage))
+
+#boolen
+is_student=True
+print(type(is_student))
+
+#boolean
+age= 19
+print(type(age))
+
+print(name,roll_number ,percentage,is_student,age)
+
